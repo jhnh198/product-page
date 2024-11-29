adc32a648e75bab0ba0c3dbf5f23f08101e71451
jhnh198 <jhnh198@gmail.com>
2024-11-29 15:52:40 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//todo: add to cart function
+
+//todo: product will be an object with a name, price, and image
+let cart = [];
+
+function addToCart(product) {
+    cart.push(product);
+    console.log(cart);
+}
+
